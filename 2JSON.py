0ce006251f980061028c86000c15eab8a8b3c82f
Pablolopezferrer <pablolopezferrer217@gmail.com>
2026-03-29 13:55:00 +0200

    Delete 2JSON.py

--- 2JSON.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import json
-df =pd.read_excel('AGP_EXCEL.xlsx')
-df = df.replace([np.inf, -np.inf], None)
-df = df.where(pd.notnull(df), None)
-datos=df.to_dict(orient='records')
-with open('datos.json', 'w') as f:
-    json.dump(datos, f, indent=4)
